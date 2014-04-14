561ffdce0a14041290b80b2e8b7d401792a6323a
louis cabrera <louis.cabrera@uponamap.com>
2014-04-14 19:27:43 +0200

    remove a bug

--- scripts/game/functions/game_loop.js
+++ scripts/game/functions/game_loop.js
@@ -1,11 +1,10 @@
 define(["RAF", "Game","game/functions/gamepad_controller"], function (RAF, Game, gamepad){
 
     function gameLoop (){
-        if(Game.state == "game")
+        if(Game.state == "game"){
             RAF(gameLoop);
         }
         Game.canvas.background.context.fillRect(0, 0, Game.canvas.background.width, Game.canvas.background.height);
-        
     }
 
     Game.states["game"] = gameLoop;
