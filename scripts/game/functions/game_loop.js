657ebcce411d3b4624aa4d9309aa6c4fb763b79a
grifdail <grifdail@gmail.com>
2014-04-14 19:17:29 +0200

    fixed bug

--- scripts/game/functions/game_loop.js
+++ scripts/game/functions/game_loop.js
@@ -1,11 +1,9 @@
 define(["RAF", "Game","game/functions/gamepad_controller"], function (RAF, Game, gamepad){
-    var controller = new gamepad({} || 0);
 
     function gameLoop (){
         if(Game.state == "game") {
             RAF(gameLoop);
         }
-        controller.update();
         Game.canvas.background.context.fillRect(0, 0, Game.canvas.background.width, Game.canvas.background.height);
     }
 
