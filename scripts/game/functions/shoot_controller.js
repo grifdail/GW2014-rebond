2480c9a5967efed4ddc64f6d1a95de2b39661e35
grifdail <grifdail@gmail.com>
2014-04-15 15:41:56 +0200

    added different rotation for cannon

--- scripts/game/functions/shoot_controller.js
+++ scripts/game/functions/shoot_controller.js
@@ -6,15 +6,16 @@ define([], function(){
         var fn = function(vx,vy) {
             if (timer<0) {
                 timer = freq;
-                var nx = this.pos.x+this.radius+Math.cos(this.rotation)*45-5;
-                var ny = this.pos.y+this.radius+Math.sin(this.rotation)*45-5;
-                manager.create(nx, ny, 10, this.rotation, 20, this.color,this.vel);
+                var nx = this.pos.x+this.radius+Math.cos(fn.rotation)*45-5;
+                var ny = this.pos.y+this.radius+Math.sin(fn.rotation)*45-5;
+                manager.create(nx, ny, 10, fn.rotation, 20, this.color,this.vel);
             }
-            
         }
+        
         fn.update = function() {
             timer--;
         }
+        fn.rotation = 0;
         return fn;
     }
 });
