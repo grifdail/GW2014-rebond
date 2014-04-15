f343cb642a9abc7b6a3890bf08996c7244ad7b8d
louis cabrera <louis.cabrera@uponamap.com>
2014-04-15 10:05:43 +0200

    collision engine

--- scripts/config.js
+++ scripts/config.js
@@ -3,10 +3,10 @@ require.config({
 	paths: {
 		"socketio" 				: '../socket.io/socket.io',
 		"RAF"	    			: "libs/requestAnimFrame",
+		"collisionEngine"		: "libs/collisionEngine"
 		"Game"					: "game/game",
 		"gameLoop"				: "game/functions/game_loop",
 		"menuLoop"				: "game/functions/menu_loop",
-		"collisionController"	: "game/functions/collision_controller",
 	},
 	shim: {
 	    'socketio': {
