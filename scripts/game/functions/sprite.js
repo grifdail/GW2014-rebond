22e4dbebbe382f69c142012a6b978b310a0cc601
0Simba <b.dersoir@student.isartdigital.com>
2014-04-15 12:12:32 +0200

    Ajout avancé systeme de sprite. Structure prete. Reste a coder et integrer dans l'engine de render

--- scripts/game/functions/sprite.js
+++ scripts/game/functions/sprite.js
@@ -1,7 +1,12 @@
-define([], function (){
+define(["libs/utils"], function (utils){
 	var Sprite = function(image, config){
-		for (var key in config)
-			this.config[key] = config[key];
+		config = utils.httpGetData(config);
+		this.animation = {};
+		for (var name in config){
+			this.animation[name] = {};
+			for (var key in config[name])
+				this.animation[name][key] = config[name][key];
+		}
 		this.image = new Image(image);
 	}
 
