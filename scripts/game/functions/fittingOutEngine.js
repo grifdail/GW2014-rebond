1af9579caf9ecb89daef24c6c1132372de895477
0Simba <b.dersoir@student.isartdigital.com>
2014-04-15 23:09:02 +0200

    Ajout collision des bullets sur les murs

--- scripts/game/functions/fittingOutEngine.js
+++ scripts/game/functions/fittingOutEngine.js
@@ -3,7 +3,7 @@ define(["game/functions/basicObject", "collisionEngine", "game/functions/renderE
 
 	}
 	FittingOutEngine.prototype.init = function(){
-		collisionEngine.addGroup("fittingOut", ["bullets"], null); 
+		collisionEngine.addGroup("fittingOut", null, null); 
 
 		this.renderEngine = new RenderEngine();
 		this.renderEngine.addGroup("fittingOut", "bullets");
@@ -11,7 +11,7 @@ define(["game/functions/basicObject", "collisionEngine", "game/functions/renderE
 		// this.create(400, 400, "bumper");
 		this.create(0,0,"wall",1920, 50);
 		this.create(0,0,"wall", 50, 1080);
-		this.create(0,1030, "wall",1920, 50 );
+		this.create(0,1030, "wall",1920, 50);
 		this.create(1870, 0, "wall", 50, 1080);
 	}
 	FittingOutEngine.prototype.create = function(x, y, type, width, height){
@@ -29,6 +29,20 @@ define(["game/functions/basicObject", "collisionEngine", "game/functions/renderE
 		else if (type == "wall"){
 			var wall = {};
 			basicObject.rect(wall, x, y, width, height);
+			var that = wall;
+			// collisionEngine.addHitbox(wall, x, y, width, height);
+			collisionEngine.addElement(wall, "fittingOut");
+
+			// wall.on("test", wall.collision, wall);
+
+			wall.reaction = function(target){
+				// var direction = Math.atan2(target.vel.y, target.vel.x);
+				if (target.pos.x - target.radius <= this.pos.x + this.width && target.pos.x + target.radius > this.pos.x + this.width || //Collision vers la gauche
+					target.pos.x + target.radius >= this.pos.x && target.pos.x - target.radius < wall.pos.x)
+					target.vel.x *= -1;
+				else
+					target.vel.y *= -1;
+			}
 			this.renderEngine.addElement("fittingOut", wall);
 		}	
 		else
