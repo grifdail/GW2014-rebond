39a0ddd917d80e5d820a9f6648df54045e25a331
grifdail <grifdail@gmail.com>
2014-04-16 12:47:09 +0200

    fixed xbox-controller

--- scripts/game/functions/player.js
+++ scripts/game/functions/player.js
@@ -26,10 +26,13 @@ define([
         this.rotationAsVec = true;
         this.gamepadController = GamepadController(pad || 0,4);
         this.physicControler = PhysicControler(0.70);
-        this.shoot = ShootController(game,0);
+        this.shoot = ShootController(game,20);
+
         this.sprite = game.renderEngine.getSprite("tank_"+color,"stay");
+
         this.canon = basicObject.basic({}, 0, 0, 96, 96, 32);
         this.canon.sprite = game.renderEngine.getSprite("tank_"+color,"canon");
+
         collisionEngine.addElement(this, "players");
         this.on("collisionEnter", player_collision, this);
         this.on("inboxOut", playerOutOfBound, this);
