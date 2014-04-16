c44ccfbc9f9ecaca563a454a28da0c10677bacd9
0Simba <b.dersoir@student.isartdigital.com>
2014-04-16 17:29:51 +0200

    Ajout gestion couleur et etat des bullets

--- scripts/game/functions/fittingOutEngine.js
+++ scripts/game/functions/fittingOutEngine.js
@@ -13,7 +13,7 @@ define(["game/functions/basicObject", "collisionEngine", "game/functions/renderE
 		this.create(0,0,"wall", 100, 1080);
 		this.create(0,980, "wall",1920, 100);
 		this.create(1820, 0, "wall", 100, 1080);
-		this.create(700, 700, "wall", 100, 100);
+		// this.create(700, 700, "wall", 100, 100);
 
 	}
 	FittingOutEngine.prototype.create = function(x, y, type, width, height){
@@ -31,6 +31,7 @@ define(["game/functions/basicObject", "collisionEngine", "game/functions/renderE
 		else if (type == "wall"){
 			var wall = {};
 			basicObject.rect(wall, x, y, width, height);
+			wall.color = "rgba(0,0,0,0)";
 			var that = wall;
 			// collisionEngine.addHitbox(wall, x, y, width, height);
 			collisionEngine.addElement(wall, "fittingOut");
