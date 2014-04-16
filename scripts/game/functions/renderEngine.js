06553c03a2f0cf502149dca40e486b7e03e86bb0
0Simba <b.dersoir@student.isartdigital.com>
2014-04-16 15:46:28 +0200

    Ajut bonne remanance bon groupe des players

--- scripts/game/functions/renderEngine.js
+++ scripts/game/functions/renderEngine.js
@@ -61,7 +61,6 @@ define(["libs/utils"], function (utils){
 	}
 
 	Renderer.prototype.addCanvas = function(name, canvas){
-		console.log(name + " canvas : " + canvas);
 		if (!this.canvas[name])
 			this.canvas[name] = canvas;
 		else
@@ -168,16 +167,23 @@ define(["libs/utils"], function (utils){
 			}
 
 			if (key == "bullets"){
-				this.canvas[key].context.globalAlpha = 1;
 				this.canvas[key].context.globalCompositeOperation = "destination-in";
 				this.canvas[key].context.fillStyle = "rgba(255,0,0,0.6)";
 				this.canvas[key].context.fillRect(0,0,this.canvas[key].width,this.canvas[key].width);
 
 				this.canvas[key].context.globalCompositeOperation = "source-over";
+			}
+			else if (key == "players"){
+				this.canvas[key].context.globalAlpha = 0.6;
+				this.canvas[key].context.globalCompositeOperation = "source-in";
+				this.canvas[key].context.fillStyle = "rgba(0,0,0,0.3)";
+				this.canvas[key].context.fillRect(0,0,this.canvas[key].width,this.canvas[key].width);
+
+				this.canvas[key].context.globalCompositeOperation = "source-over";
 				this.canvas[key].context.globalAlpha = 1;
+
 			}
 			else{
-				if (!key == "players")
 				this.canvas[key].context.clearRect(0,0,this.canvas[key].width,this.canvas[key].width);
 			}
 			// this.canvas[key].context.globalAlpha = 1;
