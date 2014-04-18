87caae8c69d8156fbb96c7df4c3f441eaa019bb4
grifdail <grifdail@gmail.com>
2014-04-18 09:35:08 +0200

    reglage audio

--- scripts/game/functions/audio.js
+++ scripts/game/functions/audio.js
@@ -6,25 +6,25 @@ define(["eventBus","libs/howler"], function(eventBus){
             "explosion":createAudio("audio/EXPLOSION.ogg",0.8),
             "tir":createAudio("audio/TIR.ogg"),
             "change_color":createAudio("audio/color_change.ogg"),
-            "perso_blue":createAudio("audio/SOUND_SELECTION_BLEU.ogg"),
-            "perso_red":createAudio("audio/SOUND_SELECTION_ROUGE.ogg"),
-            "perso_yellow":createAudio("audio/SOUND_SELECTION_JAUNE.ogg"),
-            "perso_green":createAudio("audio/SOUND_SELECTION_VERT.ogg"),
-            "commentary1":createAudio("audio/COMMENTAIRES/commentaire1.ogg"),
-            "commentary2":createAudio("audio/COMMENTAIRES/commentaire2.ogg"),
-            "commentary3":createAudio("audio/COMMENTAIRES/commentaire3.ogg"),
-            "commentary4":createAudio("audio/COMMENTAIRES/commentaire4.ogg"),
-            "commentary5":createAudio("audio/COMMENTAIRES/commentaire5.ogg"),
-            "commentary6":createAudio("audio/COMMENTAIRES/commentaire6.ogg"),
-            "commentary7":createAudio("audio/COMMENTAIRES/commentaire7.ogg"),
-            "commentary8":createAudio("audio/COMMENTAIRES/commentaire8.ogg"),
-            "commentary9":createAudio("audio/COMMENTAIRES/commentaire9.ogg"),
-            "commentary10":createAudio("audio/COMMENTAIRES/commentaire10.ogg"),
-            "commentary11":createAudio("audio/COMMENTAIRES/commentaire13.ogg"),
-            "commentary12":createAudio("audio/COMMENTAIRES/commentaire14.ogg"),
-            "commentary13":createAudio("audio/COMMENTAIRES/commentaire15.ogg"),
-            "musicGame":createAudio("audio/musiques/MUSIQUE DE LA PARTIE.ogg"),
-            "musicMenu":createAudio("audio/musiques/MUSIQUE ECRAN TITRE+selectiondeperso.ogg"),
+            "perso_blue":createAudio("audio/SOUND_SELECTION_BLEU.ogg",2),
+            "perso_red":createAudio("audio/SOUND_SELECTION_ROUGE.ogg",2),
+            "perso_yellow":createAudio("audio/SOUND_SELECTION_JAUNE.ogg",2),
+            "perso_green":createAudio("audio/SOUND_SELECTION_VERT.ogg",2),
+            "commentary1":createAudio("audio/COMMENTAIRES/commentaire1.ogg",3),
+            "commentary2":createAudio("audio/COMMENTAIRES/commentaire2.ogg",3),
+            "commentary3":createAudio("audio/COMMENTAIRES/commentaire3.ogg",3),
+            "commentary4":createAudio("audio/COMMENTAIRES/commentaire4.ogg",3),
+            "commentary5":createAudio("audio/COMMENTAIRES/commentaire5.ogg",3),
+            "commentary6":createAudio("audio/COMMENTAIRES/commentaire6.ogg",3),
+            "commentary7":createAudio("audio/COMMENTAIRES/commentaire7.ogg",3),
+            "commentary8":createAudio("audio/COMMENTAIRES/commentaire8.ogg",3),
+            "commentary9":createAudio("audio/COMMENTAIRES/commentaire9.ogg",3),
+            "commentary10":createAudio("audio/COMMENTAIRES/commentaire10.ogg",3),
+            "commentary11":createAudio("audio/COMMENTAIRES/commentaire13.ogg",3),
+            "commentary12":createAudio("audio/COMMENTAIRES/commentaire14.ogg",3),
+            "commentary13":createAudio("audio/COMMENTAIRES/commentaire15.ogg",3),
+            "musicGame":createAudio("audio/musiques/MUSIQUE DE LA PARTIE.ogg",0.5),
+            "musicMenu":createAudio("audio/musiques/MUSIQUE ECRAN TITRE+selectiondeperso.ogg",0.5),
         };
 
         eventBus.on("play explosion",function() {
@@ -49,7 +49,7 @@ define(["eventBus","libs/howler"], function(eventBus){
         });
 
         eventBus.on("play death commentary",function() {
-            if (Math.random()>0.33) {
+            if (Math.random()>0.5) {
                 return
             };
             var m = Math.floor(Math.random()*13+1);
